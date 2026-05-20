e9c59cf25083087302dc98ae84ef6b75d78c8848
Riadul Islam <riadul551@gmail.com>
2026-05-21 03:14:23 +0600

    feat: enhance DetailedIdea component with detailed view layout and navigation

--- src/app/(Shared)/ideas/[id]/page.jsx
+++ src/app/(Shared)/ideas/[id]/page.jsx
@@ -1,25 +1,109 @@
+import { ArrowLeft, PinFill } from "@gravity-ui/icons";
+import { Button, Calendar } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const DetailedIdea = async ({ params }) => {
   const { id } = await params;
-  console.log("id", id);
+  //   console.log("id", id);
+  const res = await fetch(`http://localhost:5000/ideas/details/${id}`);
+  const ideaDetails = await res.json();
 
-  //   const {
-  //     _id,
-  //     title,
-  //     tag,
-  //     solution,
-  //     shortDescription,
-  //     problem,
-  //     price,
-  //     imageUrl,
-  //     detailedDescription,
-  //     createdBy,
-  //     category,
-  //     audience,
-  //   } = idea;
+  //   console.log(ideaDetails);
 
-  return <div>detailed idea</div>;
+  const {
+    _id,
+    title,
+    tag,
+    solution,
+    shortDescription,
+    problem,
+    price,
+    imageUrl,
+    detailedDescription,
+    createdBy,
+    category,
+    audience,
+  } = ideaDetails;
+
+  return (
+    <div className="container mx-auto py-9 px-2 ">
+      {/* nav button  */}
+      <div className="flex justify-between items-center mb-5 ">
+        <div>
+          <Link href={"/destinations"} className="flex items-center gap-2">
+            <ArrowLeft></ArrowLeft> Back to Ideas
+          </Link>
+        </div>
+        {/* <div className="space-x-2">
+          <EditDestination detailsData={detailsData}></EditDestination>
+          <DeleteDestination detailsData={detailsData}></DeleteDestination>
+        </div> */}
+      </div>
+      {/* details section  */}
+      <div className="text-gray-600">
+        <div className="flex flex-col lg:flex-row  items-start justify-center gap-7">
+          <div className="w-full lg:w-[50%]  h-50 md:h-75 lg:h-100   overflow-hidden rounded-xl ">
+            <Image
+              src={imageUrl}
+              alt={title}
+              width={1080}
+              height={1080}
+              className="w-full h-full object-cover  "
+            ></Image>
+          </div>
+          <div className="w-full  lg:w-[50%] flex flex-col lg:flex-row justify-between items-start gap-7  py-3 ">
+            <div>
+              <h2 className="card-title font-bold text-lg md:text-xl">
+                {title}
+              </h2>
+
+              <h1 className="mt-2 text-sm ">{shortDescription}</h1>
+
+              <p className="my-3 text-sm text-orange-500 bg-base-300 inline-block py-1 px-6 rounded-full  ">
+                {category}
+              </p>
+              <p className="my-3 ">
+                <span className="font-semibold ">Targeted Audience: </span>
+                {audience}.
+              </p>
+              <p>
+                To implement this idea commercially, need around{" "}
+                <span className="font-semibold text-xl ">${price}</span>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-9 ">
+          <h1 className="text-lg font-semibold text-black ">
+            Details Overview
+          </h1>
+          <p>{detailedDescription}</p>
+          <div className="details-footer flex flex-col lg:flex-row justify-center items-center gap-3 mt-4 ">
+            <div className="p-2 w-full lg:w-[50%] border border-orange-300 rounded-xl">
+              <h3 className=" font-semibold">Problem Statement</h3>
+              <p className="text-sm">{problem}</p>
+            </div>
+            <div className="p-2 w-full lg:w-[50%] border border-orange-300  rounded-xl">
+              <h3 className=" font-semibold">Solutions</h3>
+              <p className="text-sm">{solution}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="py-9 ">
+        <Link href={"/myBookings"}>
+          <Button
+            variant="outline"
+            className={"w-full text-cyan-500 border-cyan-500 rounded "}
+          >
+            Show Your Booking Destinations
+          </Button>
+        </Link>
+      </div> */}
+    </div>
+  );
 };
 
 export default DetailedIdea;
