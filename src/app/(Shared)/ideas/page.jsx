09e918adac04a3e542b6f137ba4cf02d1f1ecf76
Riadul Islam <riadul551@gmail.com>
2026-05-20 18:56:05 +0600

    feat: implement IdeaPage component with idea fetching and display using IdeaCard

--- src/app/(Shared)/ideas/page.jsx
+++ src/app/(Shared)/ideas/page.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import IdeaCard from "@/component/IdeaCard";
+import React from "react";
 
-const IdeaPage = () => {
+const IdeaPage = async () => {
+  const res = await fetch("http://localhost:5000/ideas");
+  const ideaData = await res.json();
+
+  console.log(ideaData);
   return (
-    <div>
-      Idea
+    <div className="container mx-auto px-7">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl text-center mt-12 mb-4">
+        Explore Ideas
+      </h1>
+      <p className="text-center text-gray-600 mb-12">
+        Choose your startup idea, explore it, interact with the idea
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {ideaData.map((idea) => {
+          <IdeaCard key={idea._id} idea={idea}></IdeaCard>
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default IdeaPage
+export default IdeaPage;
---
+++ src/component/IdeaCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IdeaCard = ({idea}) => {
+    console.log(idea)
+  return (
+    <div>
+      ideaCard
+    </div>
+  )
+}
+
+export default IdeaCard
