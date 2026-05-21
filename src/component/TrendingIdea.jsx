c796ab637aec38cdb428232a7187d3e5c6acf85b
Riadul Islam <riadul551@gmail.com>
2026-05-21 20:13:10 +0600

    feat: update TrendingIdea component title and styling for improved presentation

--- src/component/TrendingIdea.jsx
+++ src/component/TrendingIdea.jsx
@@ -6,12 +6,9 @@ const TrendingIdea = async () => {
   const ideaData = await res.json();
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl text-center mt-12 mb-4">
-        Explore Ideas
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mt-36  mb-12  ">
+        Trending Ideas
       </h1>
-      <p className="text-center text-gray-600 mb-12">
-        Choose your startup idea, explore it, interact with the idea
-      </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 items-stretch ">
         {ideaData.map((idea) => (
           <IdeaCard key={idea._id} idea={idea}></IdeaCard>
